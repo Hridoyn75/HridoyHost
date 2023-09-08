a7382131b0109a0bf274ee36b819f3244fc86b37
Nazmul Hridoy <hridoyn75@gmail.com>
2023-09-08 17:25:36 +0600

    added real backend and update ui

--- src/app/page.js
+++ src/app/page.js
@@ -12,8 +12,13 @@ export default function Home() {
   }
   return (
     <main className=" w-full h-screen flex flex-col justify-center items-center">
-        <form onSubmit={handleSubmit}  className=' bg-[#FF3D3D] rounded-lg p-10 flex flex-col justify-center gap-5'>
-         <input onChange={(e)=> setChatID(e.target.value)} placeholder='Enter your ChatID' required type="number" className=' placeholder:text-blue-700 text-black border-2 border-[#1d2e3d] p-2 rounded focus:outline-none bg-[#4a9d9c]'/>
+        
+        <form onSubmit={handleSubmit}  className=' bg-[#FF3D3D] rounded-lg p-10 pt-5 text-center flex flex-col justify-center gap-5'>
+        <div>
+          <h1 className=' text-2xl text-[#0D1F2D] font-bold'>HridoyHost</h1>
+          <p className=' text-sm text-blue-950'>Unlimited spcae for your Memories</p>
+        </div>
+          <input onChange={(e)=> setChatID(e.target.value)} placeholder='Enter your ChatID' required type="number" className=' placeholder:text-blue-700 text-black border-2 border-[#1d2e3d] p-2 rounded focus:outline-none bg-[#4a9d9c]'/>
           <button type="submit" className=' text-center hover:bg-[#1d2e3d] bg-[#0D1F2D] px-4 py-2 rounded-md'>Get Entry</button>
         </form>
     </main>
--- src/app/user/[chatID]/page.js
+++ src/app/user/[chatID]/page.js
@@ -2,23 +2,25 @@ import Image from "next/image";
 import Link from "next/link";
 
 const UserPage = async ({params}) => {
-    const response = await fetch("http://localhost:5000/user/" + params.chatID,
+    const response = await fetch("https://server-hridoyhost.onrender.com/user/" + params.chatID,
     { cache: 'no-store' })
     const data = await response.json();
     
     return (
     <>
-    <div className=" flex flex-wrap">
+    <h1 className=" text-center text-3xl py-8">All Your Memories🚀</h1>
+    <div className=" flex flex-wrap px-2 md:px-10">
         {data.map(photo =>{
             return (
                 <div key={photo.id} className=" p-2 w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5">
-                    <div className="rounded text-center ">
+                    <div className="rounded  text-center ">
                     <Image 
+                    priority
                     src={photo.url}
                     width={1080}
                     height={1080}
                     alt="Photo"
-                    className=" object-cover rounded aspect-square" />
+                    className=" object-cover rounded aspect-square mb-2" />
                     <a href={photo.url}>Download</a>
                     </div>
                 </div>
